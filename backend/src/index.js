5cfa370039f3699913722fb7f641c4659b704195
Sayyed Sufiyan <sufiyan.sayed92@gmail.com>
2025-01-06 02:28:39 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,9 @@ import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
 import { server, app } from "./lib/socket.js";
 
+import path from 'path';
+
+
 dotenv.config();
 //middleware handler
 app.use(express.json({ limit: "10mb" })); // Allows large payloads for Base64 images
@@ -22,6 +25,16 @@ app.use("/api/messages", messageRoutes);
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    );
+}
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on port: ${PORT}`);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api", // server route should be put here
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",// server route should be put here
     withCredentials: true,
 });
